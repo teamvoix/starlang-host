de5c7c43c49dfac69db59b3042db8700c27e885f
Wave <morethanicloud@icloud.com>
2024-06-26 11:54:22 +0500

    Alpha version

---
+++ assets/i18n.5f0171eb.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index.1e265679.js";import{c as r}from"./vue-i18n.runtime.71015a32.js";var t={},c=e(({app:o})=>{const a=r({locale:"ru",globalInjection:!0,messages:t});o.use(a)});export{c as default};
