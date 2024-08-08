1deb0ebfe7b5be897008cfef6b2b9214cc3f91c9
Wave <morethanicloud@icloud.com>
2024-08-09 01:05:39 +0500

    Navbar label

---
+++ assets/i18n.c80385e4.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index.eab0b9ef.js";import{c as r}from"./vue-i18n.runtime.6821e94b.js";var t={},c=e(({app:o})=>{const a=r({locale:"ru",globalInjection:!0,messages:t});o.use(a)});export{c as default};
