9830e122bc3b98f8df51031c9309f53ab86364e7
Wave <morethanicloud@icloud.com>
2024-11-14 11:14:16 +0500

    Minor fixes

---
+++ assets/i18n.864e37bf.js
@@ -0,0 +1,1 @@
+import{b as e}from"./index.e2fe68e0.js";import{c as r}from"./vue-i18n.runtime.9f8a55e0.js";var t={},c=e(({app:o})=>{const a=r({locale:"ru",globalInjection:!0,messages:t});o.use(a)});export{c as default};
